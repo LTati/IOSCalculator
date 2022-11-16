2ccc820f217c2313501e450fd45b6c1b0c34634b
Ltati <proksi_n@mail.ru>
2022-11-16 12:40:48 +0300

    Initial Commit

---
+++ IOSCalculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IOSCalculator
+//
+//  Created by user on 16/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
